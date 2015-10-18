26ad9539017adb936c1380f7855da3bf38ca0e7a
ajith23 <23ajith7@gmail.com>
2015-10-18 14:46:55 -0700

    Data class for Cstm Entity

---
+++ SBS/Data/CstmD.cs
@@ -0,0 +1,38 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public static class CstmD
+    {
+        public static Cstm Read(string connectionString, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DataSet ReadAll(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Create(string connectionString, Cstm dataObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool Update(string connectionString, Cstm dataObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool Delete(string connectionString, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
