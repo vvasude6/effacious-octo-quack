1585de66fb2337362a6e3f5694807dbacb041237
ajith23 <23ajith7@gmail.com>
2015-10-18 14:48:41 -0700

    Included Classes for processing Entity Data

---
+++ SBS/Data/ErrmD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Data;
+
+namespace Data
+{
+    public static class ErrmD
+    {
+    }
+}
