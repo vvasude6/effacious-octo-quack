a5d171a6d71a023db2d5b2e1e8cef8dfeec4d69b
sojourner6 <sarkarsom10@gmail.com>
2015-10-18 06:09:08 +0530

    Added Entity v3.0

---
+++ SBS/Entity/Errm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    class Errm
+    {
+        /* 
+         *          Error Codes
+         * -----------------------------------------------
+         * 000: System Error (to be thrown for errors not in ERRM table)
+         * 010: Low privilege
+         * 011: Account not found
+         */
+        // ****** if error not found in ERRM, return some text instead of error code ******
+        /*String SPROC = Mnemonics.SP_ERRM_ALL;
+        String err_id;
+        String err_desc;
+        public Errm(String errCode)
+        {
+            Db fetch = new Db(SPROC);
+        }
+        public String getData()
+        {
+            return this.err_desc;
+        }*/
+    }
+}
