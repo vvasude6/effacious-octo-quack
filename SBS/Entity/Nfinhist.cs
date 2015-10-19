7fd66a842a8f94c815adda2946efbb54b6987939
sojourner6 <sarkarsom10@gmail.com>
2015-10-18 23:16:23 -0700

    revert1

--- SBS/Entity/Nfinhist.cs
+++ SBS/Entity/Nfinhist.cs
@@ -8,12 +8,26 @@ namespace Entity
 {
     public class Nfinhist
     {
-        public String apprv_empid { get; set; }
+        private static String DATE_FORMAT = "en-US";
         public String ac_no { get; set; }
         public String ref_no { get; set; }
         public String tran_date { get; set; }
         public String tran_timestamp { get; set; }
         public String tran_desc { get; set; }
         public String init_empid { get; set; }
+        public String apprv_empid { get; set; }
+        public Nfinhist()
+        { }
+        public Nfinhist(String acno, String refno, String trandesc, String initempid, String apprvempid)
+        {
+            DateTime dt = DateTime.Now;
+            this.ac_no = acno;
+            this.ref_no = refno;
+            this.tran_date = dt.Date.ToString(Nfinhist.DATE_FORMAT);
+            this.tran_timestamp = dt.ToString(Nfinhist.DATE_FORMAT);
+            this.tran_desc = trandesc;
+            this.init_empid = initempid;
+            this.apprv_empid = apprvempid;
+        }
     }
 }
