751b4349cc125c795948f7b4b6be355601aa6490
jonlammers <jonlammers66@gmail.com>
2015-10-24 13:23:58 -0700

    Working Validity global class and test code.

--- SBS/UITests/ValidateTests.cs
+++ SBS/UITests/ValidateTests.cs
@@ -15,7 +15,7 @@ namespace UI.Tests
         public void generalValidateTest()
         {
             // Check Alphabetic
-            Assert.IsTrue(Validate.generalValidate("AaBbCcDdEeFfGg",true,false));
+            Assert.IsTrue(Validate.generalValidate("AaBbCcDdEeFfGg", true, false));
 
             Assert.IsFalse(Validate.generalValidate("AaBbCc7DdEeFfGg", true, false));
             Assert.IsFalse(Validate.generalValidate("AaBbCc DdEeFfGg", true, false));
@@ -36,13 +36,157 @@ namespace UI.Tests
 
             // Check Special
             Assert.IsTrue(Validate.generalValidate("!@#$%^&*()<>", false, false, "!@#$%^&*()<>"));
-            Assert.IsTrue(Validate.generalValidate("!@#$%^&*()<>", false, false, "!@#$%^&*)<>"));
+            Assert.IsTrue(Validate.generalValidate("!@#$%^&*()<>", false, false, "!@#$%^&*()<>"));
 
             Assert.IsFalse(Validate.generalValidate("!@#$2^&*()<>", false, false, "!@#$%^&*()<>"));
             Assert.IsFalse(Validate.generalValidate("A", false, false, "!@#$%^&*()<>"));
             Assert.IsFalse(Validate.generalValidate(" ", false, false, "!@#$%^&*()<>"));
             Assert.IsFalse(Validate.generalValidate("-", false, false, "!@#$%^&*()<>"));
             Assert.IsFalse(Validate.generalValidate("!@#$ ^&*()<>", false, false, "!@#$%^&*()<>"));
+        }
+
+        [TestMethod()]
+        public void isUserNameValidTest()
+        {
+            Assert.IsTrue(Validate.isUserNameValid("Jon"));
+            Assert.IsTrue(Validate.isUserNameValid("Marie-Ann"));
+            Assert.IsTrue(Validate.isUserNameValid("Johnson Ash"));
+            Assert.IsFalse(Validate.isUserNameValid("ajslkdjf<>"));
+            Assert.IsFalse(Validate.isUserNameValid("Fred, Wilma"));
+        }
+
+        [TestMethod()]
+        public void isZipCodeValidTest()
+        {
+            Assert.IsTrue(Validate.isZipCodeValid("01234-5678"));
+            Assert.IsTrue(Validate.isZipCodeValid("98765"));
+            Assert.IsFalse(Validate.isZipCodeValid("98765 1234"));
+            Assert.IsFalse(Validate.isZipCodeValid("98a65 1234"));
+            Assert.IsFalse(Validate.isZipCodeValid("98765.1234"));
+            Assert.IsFalse(Validate.isZipCodeValid("98%65-1234"));
+            Assert.IsFalse(Validate.isZipCodeValid("98765-12340"));
+        }
+
+        [TestMethod()]
+        public void isCityValidTest()
+        {
+            Assert.IsTrue(Validate.isCityValid("Phoenix"));
+            Assert.IsTrue(Validate.isCityValid("New York"));
+            Assert.IsTrue(Validate.isCityValid("Raliegh-Duram"));
+            Assert.IsFalse(Validate.isCityValid("Raliegh%Duram"));
+            Assert.IsFalse(Validate.isCityValid("Raliegh%Duram"));
+        }
+
+        [TestMethod()]
+        public void isStateValidTest()
+        {
+            Assert.IsTrue(Validate.isStateValid("AZ"));
+            Assert.IsTrue(Validate.isStateValid("NY"));
+            Assert.IsTrue(Validate.isStateValid("CA"));
+            Assert.IsFalse(Validate.isStateValid("ILL"));
+            Assert.IsFalse(Validate.isStateValid("12"));
+            Assert.IsFalse(Validate.isStateValid("V-"));
+            Assert.IsFalse(Validate.isStateValid("B*"));
+        }
+
+        [TestMethod()]
+        public void isPhoneNumberValidTest()
+        {
+            Assert.IsTrue(Validate.isPhoneNumberValid("+01 (480) 415-0904"));
+            Assert.IsTrue(Validate.isPhoneNumberValid("480-415-0904"));
+            Assert.IsTrue(Validate.isPhoneNumberValid("(480) 415-0904"));
+            Assert.IsFalse(Validate.isPhoneNumberValid("+01 (480) 415-0904-2341"));
+            Assert.IsFalse(Validate.isPhoneNumberValid("(480)415-0904 x234"));
+            Assert.IsFalse(Validate.isPhoneNumberValid("480%415%0904"));
+        }
+
+        [TestMethod()]
+        public void isEmailAddressValidTest()
+        {
+            Assert.IsTrue(Validate.isEmailAddressValid("jon.lammers@gd-ms.com"));
+            Assert.IsTrue(Validate.isEmailAddressValid("jon_lammers@gdc4s.com"));
+            Assert.IsFalse(Validate.isEmailAddressValid("jon lammers@gd-ms.com"));
+            Assert.IsFalse(Validate.isEmailAddressValid("jon%lammers@gd-ms.com"));
+            Assert.IsFalse(Validate.isEmailAddressValid("jon^lammers@gd-ms.com"));
+            Assert.IsFalse(Validate.isEmailAddressValid("jon$lammers@gd-ms.com"));
+            Assert.IsFalse(Validate.isEmailAddressValid("jon#lammers@gd-ms.com"));
+        }
+
+        [TestMethod()]
+        public void isUserIDValidTest()
+        {
+            Assert.IsTrue(Validate.isUserIDValid("12345"));
+            Assert.IsTrue(Validate.isUserIDValid("67890"));
+            Assert.IsFalse(Validate.isUserIDValid("Fred"));
+            Assert.IsFalse(Validate.isUserIDValid("12345C"));
+            Assert.IsFalse(Validate.isUserIDValid("123-453"));
+            Assert.IsFalse(Validate.isUserIDValid("123_453"));
+        }
+
+        [TestMethod()]
+        public void isBranchValidTest()
+        {
+            Assert.IsTrue(Validate.isBranchValid("12345"));
+            Assert.IsTrue(Validate.isBranchValid("67890"));
+            Assert.IsFalse(Validate.isBranchValid("Fred"));
+            Assert.IsFalse(Validate.isBranchValid("12345C"));
+            Assert.IsFalse(Validate.isBranchValid("123-453"));
+            Assert.IsFalse(Validate.isBranchValid("123_453"));
+        }
+
+        [TestMethod()]
+        public void isSecurityQuestionValidTest()
+        {
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Dog's, name?"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Childrens first.last name?"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Comma, dash- ?"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("*"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("x' AND 1=(SELECT COUNT(*) FROM tabname); --"));
+        }
+
+        [TestMethod()]
+        public void isSecurityAnswerValidTest()
+        {
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Maxwell-Scratchington,III"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Jessi.Lammers"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Miranda'Boo"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("*"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("x' AND 1=(SELECT COUNT(*) FROM tabname); --"));
+        }
+
+        [TestMethod()]
+        public void isDescriptionValidTest()
+        {
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Dog's, name?"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Childrens first.last name?"));
+            Assert.IsTrue(Validate.isSecurityQuestionValid("Comma, dash- ?"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("*"));
+            Assert.IsFalse(Validate.isSecurityQuestionValid("x' AND 1=(SELECT COUNT(*) FROM tabname); --"));
+        }
+
+        [TestMethod()]
+        public void isAccountNumberValidTest()
+        {
+            Assert.IsTrue(Validate.isAccountNumberValid("12345"));
+            Assert.IsTrue(Validate.isAccountNumberValid("67890"));
+            Assert.IsFalse(Validate.isAccountNumberValid("Fred"));
+            Assert.IsFalse(Validate.isAccountNumberValid("12345C"));
+            Assert.IsFalse(Validate.isAccountNumberValid("123-453"));
+            Assert.IsFalse(Validate.isAccountNumberValid("123_453"));
+        }
+
+        [TestMethod()]
+        public void isAmountValidTest()
+        {
+            Assert.IsTrue(Validate.isAmountValid("123"));
+            Assert.IsTrue(Validate.isAmountValid("678.90"));
+            Assert.IsTrue(Validate.isAmountValid("0.90"));
+            Assert.IsTrue(Validate.isAmountValid(".90"));
+            Assert.IsTrue(Validate.isAmountValid("9999999.99"));
+            Assert.IsFalse(Validate.isAmountValid("10000000.00"));
+            Assert.IsFalse(Validate.isAmountValid("12345C"));
+            Assert.IsFalse(Validate.isAmountValid("123-453"));
+            Assert.IsFalse(Validate.isAmountValid("123_453"));
         }
     }
 }
