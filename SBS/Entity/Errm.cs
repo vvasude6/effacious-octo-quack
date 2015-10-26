fecbdf5e964ba73a8ab115c2cf2110c2292ff25a
Ajith <23ajith7@gmail.com>
2015-10-26 00:08:13 -0700

    Updated error schema

--- SBS/Entity/Errm.cs
+++ SBS/Entity/Errm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace Entity
         
         public String err_id { get; set; }
         public String err_desc { get; set; }
+        public String err_code { get; set; }
         
     }
 }
