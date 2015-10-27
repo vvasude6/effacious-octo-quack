0faac00f1482513daf3d3b1466f32ac2296ce208
sojourner6 <sarkarsom10@gmail.com>
2015-10-27 11:21:32 -0700

    Customer, Merchant registration fixed

--- SBS/Business/Y_015.cs
+++ SBS/Business/Y_015.cs
@@ -37,13 +37,13 @@ namespace Business
 
         public Y_015(String txid, String connectionString, String a, String b, String c, String d, String e, String f,
             String g, String h, String i, String j, String k, String l, String m, String n, String o, String p, String q, String r,
-            String s, String t, String u, String v, String w)
+            String s, String t, String u, String v, String w, String loginAc)
         {
             this.TXID = txid;
             dberr = new Data.Dber();
             //this.cusNo = loginAc;
             
-            if (processTransaction(connectionString, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w) != 0)
+            if (processTransaction(connectionString, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, loginAc) != 0)
             {
                 this.error = true;
             }
@@ -51,7 +51,7 @@ namespace Business
         }
         private int processTransaction(String connectionString, String a, String b, String c, String d, String e, String f,
             String g, String j, String h, String i, String l, String m, String n, String k, String o, String p, String q, String r,
-            String s, String t, String u, String v, String w)
+            String s, String t, String u, String v, String w, String loginAc)
         {
             this.tx = new Cp_Txnm(connectionString, TXID, dberr);
             // Check if TXNM fetch for transaction type "010" is successful. Return if error encountered
@@ -93,82 +93,96 @@ namespace Business
             //    dberr.setError(Mnemonics.DbErrorCodes.TXERR_EXISTING_USER);
             //    return -1;
             //}
-            //pvg = new Privilege(this.txnPvga, this.usrPvga, this.txnPvgb);
-            //if (!pvg.verifyInitPrivilege(dberr))
-            //{
-            //    result = dberr.getErrorDesc(connectionString);
-            //    return -1;
-            //}
-            //if (!pvg.verifyApprovePrivilege())
-            //{
-            String inData = this.TXID + "|" + a + "|" + b + "|" + c + "|" + d + "|" + e + "|" + f +
-                "|" + g + "|" + j + "|" + h + "|" + i + "|" + l + "|" + m + "|" + n + "|" + k + "|" + o + "|" + p + "|" + q + "|" + r
-                + "|" + s + "|" + t + "|" + u + "|" + v + "|" + w;/*this.changeAmount.ToString()*/;
-                if (pvg.writeToPendingTxns(
-                    connectionString,               /* connection string */
-                    "0",                            /* account 1 */
-                    "0",                            /* account 2 */
-                    a, //initCustomer,              /* customer number */
-                    tx.txnmP.tran_pvgb.ToString(),  /* transaction approve privilege */
-                    tx.txnmP.tran_desc,             /* transaction description */
-                    "0",                            /* initiating employee number */
-                    0,                              /* debit amount */
-                    0,                              /* credit amount */
-                    tx.txnmP.tran_id,               /* transaction id (not tran code) */
-                    inData,                         /* incoming transaction string in XSwitch */
-                    dberr                           /* error tracking object */
-                    ) != 0)
+            int cusPvg = 0;
+            if (cstm.cs_type.Equals("A") || cstm.cs_type.Equals("1") || cstm.cs_type.Equals(" "))
+            {
+                cusPvg = 1;
+            }
+            else
+            {
+                if (cstm.cs_type.Equals("B") || cstm.cs_type.Equals("2"))
                 {
-                    resultP = dberr.getErrorDesc(connectionString);
+                    cusPvg = 2;
+                }
+            }
+            if (loginAc.Equals("0") || loginAc.Equals(" "))
+            {
+                pvg = new Privilege(this.txnPvga, this.txnPvgb, cusPvg);
+                if (!pvg.verifyInitPrivilege(dberr))
+                {
+                    result = dberr.getErrorDesc(connectionString);
                     return -1;
                 }
-                resultP = Mnemonics.DbErrorCodes.MSG_SENT_FOR_AUTH;
-                return 0;
-            //}
-            var data = Data.CstmD.Create(connectionString, cstm, dberr);
+                if (!pvg.verifyApprovePrivilege())
+                {
+                    String inData = this.TXID + "|" + a + "|" + b + "|" + c + "|" + d + "|" + e + "|" + f +
+                        "|" + g + "|" + j + "|" + h + "|" + i + "|" + l + "|" + m + "|" + n + "|" + k + "|" + o +
+                        "|" + p + "|" + q + "|" + r + "|" + s + "|" + t + "|" + u + "|" + v + "|" + w ;
+                    if (pvg.writeToPendingTxns(
+                        connectionString,               /* connection string */
+                        "0",                            /* account 1 */
+                        "0",                            /* account 2 */
+                        "0",                            /* customer number */
+                        tx.txnmP.tran_pvgb.ToString(),  /* transaction approve privilege */
+                        tx.txnmP.tran_desc,             /* transaction description */
+                        "0",                            /* initiating employee number */
+                        0,                              /* debit amount */
+                        0,                              /* credit amount */
+                        tx.txnmP.tran_id,               /* transaction id (not tran code) */
+                        inData,                         /* incoming transaction string in XSwitch */
+                        dberr                           /* error tracking object */
+                        ) != 0)
+                    {
+                        resultP = dberr.getErrorDesc(connectionString);
+                        return -1;
+                    }
+                    resultP = Mnemonics.DbErrorCodes.MSG_SENT_FOR_AUTH;
+                    return 0;
+                }
+            }
+            else
+            {
+                int data = Data.CstmD.Create(connectionString, cstm, dberr);
+                if (dberr.ifError())
+                {
+                    result = dberr.getErrorDesc(connectionString);
+                    return -1;
+                }
+                if (tx.txnmP.tran_fin_type.Equals("Y"))
+                {
+                    // Write to FINHIST table
+                    Entity.Finhist fhist = new Entity.Finhist(
+                        "0",                        /* Account Number */
+                        "0",                        /* Reference Number */
+                        this.tx.txnmP.tran_desc,    /* Transaction Description */
+                        0,                          /* Debit Amount */
+                        0,                          /* Credit Amount */
+                        "0",                        /* Remaining Balance */
+                        "0",                        /* Initiating Employee Id */
+                        loginAc,                    /* Approve Employee Id */
+                        cstm.cs_no                  /* Initiating Customer Number */
+                        );
+                    Data.FinhistD.Create(connectionString, fhist, dberr);
+                }
+                else
+                {
+                    // Write to NFINHIST table
+                    Entity.Nfinhist nFhist = new Entity.Nfinhist(
+                        "0",                        /* Account Number */
+                        "0",                        /* Reference Number */
+                        this.tx.txnmP.tran_desc,    /* Transaction Description */
+                        "0",                        /* Initiating Employee Id */
+                        loginAc,                    /* Approve Employee Id */
+                        cstm.cs_no                  /* Initiating Customer Number */
+                        );
+                    Data.NfinhistD.Create(connectionString, nFhist, dberr);
+                }
+            }
             if (dberr.ifError())
             {
                 result = dberr.getErrorDesc(connectionString);
                 return -1;
             }
-            
-            /*
-                if (pvg.isPending)
-                {
-                    Entity.Pendtxn pending = new Entity.Pendtxn(0, "0", this.cusNo, "0",
-                        Convert.ToString(this.txnPvgb), this.cusNo, "0", 0, this.tx.txnmP.tran_desc, this.tx.txnmP.tran_id);
-                    Data.PendtxnD.Create(connectionString, pending);
-                    if (dberr.ifError())
-                    {
-                        result = dberr.getErrorDesc(connectionString);
-                        return -1;
-                    }
-                    else
-                    {
-                        dberr.setError(Mnemonics.TxnCodes.TX_SENT_TO_APPROVER);
-                        return -1;
-                    }
-                }
-                else
-                {
-                    result = dberr.getErrorDesc(connectionString);
-                    return -1;
-                }
-            }*/
-            /*
-            if (tx.txnmP.tran_fin_type.Equals("Y"))
-            {
-                // Write to FINHIST table
-                Entity.Finhist fhist = new Entity.Finhist(cstm.cs_no, "0", this.tx.txnmP.tran_desc,
-                    0, 0, "0", "0", "0", "0");
-                Data.FinhistD.Create(connectionString, fhist, dberr);
-            }
-            else
-            {
-                // Write to NFINHIST table
-                Entity.Nfinhist nFhist = new Entity.Nfinhist(cstm.cs_no, "0", this.tx.txnmP.tran_desc, "0", "0", "0");
-                Data.NfinhistD.Create(connectionString, nFhist, dberr);
-            }*/
             resultP = "Successful!";
             return 0;
         }
