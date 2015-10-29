5f225e6f93ca096a0761b2581abe4396ca70e58d
sojourner6 <sarkarsom10@gmail.com>
2015-10-29 06:54:42 -0700

    cust reg. field mapping

--- SBS/Business/Y_026.cs
+++ SBS/Business/Y_026.cs
@@ -31,16 +31,16 @@ namespace Business
         Boolean error = false;
         public Y_026(String conectionString, String txid, String a, String b, String c, String d, String e,
             String f, String g, String h, String i, String j, String k, String l, String m, String n, String o, String p, String q,
-            String loginAc)
+            String r, String s, String t, String loginAc)
         {
             dberr = new Data.Dber();
             this.TXID = txid;
             empm = new Entity.Empm();
-            processTransaction(conectionString, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, loginAc);
+            processTransaction(conectionString, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, loginAc);
         }
         private int processTransaction(String connectionString, String a, String b, String c, String d, String e,
             String f, String g, String h, String i, string j, String k, String l, String m, String n, String o, String p, String q,
-            String loginAc)
+            String r, String s, String t, String loginAc)
         {
             this.tx = new Cp_Txnm(connectionString, this.TXID, dberr);
             if (dberr.ifError())
@@ -54,17 +54,19 @@ namespace Business
             empm.emp_lname = d;
             empm.emp_addr1 = e;
             empm.emp_addr2 = f;
-            empm.emp_city = i;
-            empm.emp_state = h;
             empm.emp_zip = g;
+            empm.emp_city = h;
+            empm.emp_state = i;
             empm.emp_brnch = j;
             empm.emp_phn = k;
             empm.emp_email = l;
-            empm.emp_secq1 = m;
-            empm.emp_ans1 = n;
-            empm.emp_secq2 = o;
-            empm.emp_ans2 = p;
-            empm.emp_pass = q;
+            empm.emp_mngr = m;
+            empm.emp_pvg = Convert.ToInt32(n);
+            empm.emp_secq1 = o;
+            empm.emp_ans1 = p;
+            empm.emp_secq2 = q;
+            empm.emp_ans2 = r;
+            empm.emp_pass = t;
             int empId = Data.EmpmD.Create(connectionString, empm, dberr);
             if (dberr.ifError())
             {
