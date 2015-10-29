06c0df9608100dbb0f43e9ec8dcf16f80a382947
Abhijit Ray <abray027@gmail.com>
2015-10-29 00:49:22 -0700

    Added Pkit

---
+++ SBS/Data/PkitD.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Data;
+
+namespace Data
+{
+    public static class PkitD
+    {
+        public static Pkit Read(string connectionString, string id, Dber dberr)
+        {
+            try
+            {
+                var PkitMasterObject = new Pkit();
+
+                var query = string.Format("select * from Pkit where id = {0}", id);
+                var data = DbAccess.ExecuteQuery(connectionString, CommandType.Text, query);
+
+                //assign the data object to Error master object
+                if (data.Tables[0].Rows.Count > 0)
+                {
+                    PkitMasterObject.id = data.Tables[0].Rows[0]["id"].ToString();
+                    PkitMasterObject.public_key = data.Tables[0].Rows[0]["public_key"].ToString();
+                    PkitMasterObject.private_key = data.Tables[0].Rows[0]["private_key"].ToString();
+                    PkitMasterObject.cs_no = data.Tables[0].Rows[0]["cs_no"].ToString();
+
+
+
+                    return PkitMasterObject;
+                }
+                else
+                {
+                    dberr.setError(Mnemonics.DbErrorCodes.DBERR_ACTM_NOFIND);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                dberr.setError(Mnemonics.DbErrorCodes.DBERR_ACTM_NOFIND);
+                throw (new Exception("Transaction Error: " + Mnemonics.DbErrorCodes.DBERR_ACTM_NOFIND));
+            }
+        }
+
+        public static int Create(string connectionString, Pkit dataObject, Dber dberr)
+        {
+            try
+            {
+
+                var query = string.Format(@"INSERT INTO [SBS].[dbo].[PKIT]
+           ([PUBLIC_KEY]
+           ,[PRIVATE_KEY]
+           ,[CS_NO])
+                    OUTPUT INSERTED.id        
+                    VALUES
+                    ('{0}'  ,'{1}','{2}' )",
+               dataObject.public_key, dataObject.private_key, dataObject.cs_no);
+                return (int)DbAccess.ExecuteScalar(connectionString, CommandType.Text, query);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool Update(string connectionString, Pkit dataObject, Dber dberr)
+        {
+            var query = string.Format(@"UPDATE [SBS].[dbo].[PKIT]
+                                       SET 
+                                           [PUBLIC_KEY] =       '{0}'
+                                          ,[PRIVATE_KEY] =       '{1}'
+                                          
+                                          
+                                          
+                                     WHERE CS_NO = {2}", dataObject.public_key, dataObject.private_key, dataObject.cs_no);
+            return DbAccess.ExecuteNonQuery(connectionString, CommandType.Text, query) == 1;
+        }
+
+        public static bool Delete(string connectionString, string cs_no, Dber dberr)
+        {
+            try
+            {
+                var query = string.Format("delete from pkit where cs_no = {0}", cs_no);
+                return DbAccess.ExecuteNonQuery(connectionString, CommandType.Text, query) == 1;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string GetBankPrivateKey(String connectionString)
+        {
+
+            string p = (string.Format("select private_key FROM Pkit WHERE id = '{0}'", '1'));
+            var output = DbAccess.ExecuteScalar(connectionString, CommandType.Text, p);
+            return output.ToString();
+        }
+        public static string GetBankPublicKey(String connectionString)
+        {
+
+            string p = (string.Format("select public_key FROM Pkit WHERE id = '{0}'", '1'));
+            var output = DbAccess.ExecuteScalar(connectionString, CommandType.Text, p);
+            return output.ToString();
+        }
+        public static string GetCustomerPublicKey(String connectionString, string cs_no)
+        {
+
+            string p = (string.Format("select public_key FROM Pkit WHERE cs_no = '{0}'", cs_no));
+            var output = DbAccess.ExecuteScalar(connectionString, CommandType.Text, p);
+            return output.ToString();
+
+        }
+
+        public static string GetCustomerPrivateKey(String connectionString, string cs_no)
+        {
+
+            string p = (string.Format("select private_key FROM Pkit WHERE cs_no = '{0}'", cs_no));
+            var output = DbAccess.ExecuteScalar(connectionString, CommandType.Text, p);
+            return output.ToString();
+
+        }
+    }
+}
---
+++ SBS/Entity/Pkit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Pkit
+    {
+        public string id { get; set; }
+        public string public_key { get; set; }
+        public string private_key { get; set; }
+
+        public string cs_no { get; set; }
+
+
+    }
+}
