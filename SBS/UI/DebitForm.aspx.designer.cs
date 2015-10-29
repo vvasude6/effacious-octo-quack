e995cddd810222bba34f34487dd7a5aa1e82b981
ajith23 <23ajith7@gmail.com>
2015-10-29 02:01:53 -0700

    Virtual keyboard for otp in deposit page

--- SBS/UI/DebitForm.aspx.designer.cs
+++ SBS/UI/DebitForm.aspx.designer.cs
@@ -92,5 +92,14 @@ namespace UI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton ResendOTPLink;
+        
+        /// <summary>
+        /// VirtualCheckBoxArea control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl VirtualCheckBoxArea;
     }
 }
